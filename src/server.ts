b93564f90205f348d3be52d4d016e64147e7de9c
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-10-09 21:33:55 -0300

    Socket.io configured

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+import express from 'express';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+
+const app = express();
+
+const server = createServer(app);
+
+const io = new Server(server);
+
+io.on('connection', async (socket) => {
+  console.log('Socket', socket.id);
+});
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'Hellow Websocket',
+  });
+});
+
+server.listen(3000, () => {
+  console.log('🎮 Server is running');
+});
