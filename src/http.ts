8f70158234332c83de64971c6bbb231edcf5f492
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-10-09 22:09:37 -0300

    Mongodb's schemas created

--- src/http.ts
+++ src/http.ts
@@ -1,11 +1,17 @@
 import path from 'path';
 import express from 'express';
+import mongoose, { ConnectOptions } from 'mongoose';
 import { Server } from 'socket.io';
 import { createServer } from 'http';
 
 const app = express();
 
 const server = createServer(app);
+
+mongoose.connect('mongodb://localhost/chatsocket', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as ConnectOptions);
 
 const io = new Server(server);
 
